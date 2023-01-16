62c9eff5fe9d7f65d42e35746ff570f50505be9e
Joel-VT <joelvarghese90@gmail.com>
2023-01-16 12:31:22 -0500

    Slight Chnages to #33

--- Blind 75/1_33_Search_in_Rotated_Sorted_Array.js
+++ Blind 75/1_33_Search_in_Rotated_Sorted_Array.js
@@ -4,26 +4,28 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    /**
- * @param {number[]} nums
- * @return {number}
- */
-    if (nums.length === 1 || nums[0] < nums[nums.length - 1]) return nums[0];
-    if (nums[nums.length - 1] < nums[nums.length - 2]) return nums[nums.length-1];
     let left = 0;
     let right = nums.length - 1;
     while (left<=right) {
         let mid = Math.floor(nums.length / 2);
         if (nums[mid] === target) return mid;
-        else if (nums[left] === target) return left;
-        else if (nums[right] === target) return right;
-        else if (nums[mid] < target) left = mid + 1; 
-        else if (nums[mid] > target) right = mid - 1;
-        else break;
+        
+        // Check if the left side is sorted
+        if(nums[left]<=nums[mid]){
+            // checking if the target is on the left side of mid
+            if(nums[left] <= target && target <= nums[mid]) right = mid-1;
+            // if target is on the right side
+            else left = mid+1;
+        }
+        // The right is sorted
+        else{
+            if(nums[mid] <= target && target <= nums[right]) left = mid+1;
+            else right = mid-1;
+        }
     }
     return -1;
 };
 
 let arr = [4,5,6,7,8,0,1,2]
 
-console.log(search(arr, 3));
+console.log(search(arr, 8));
